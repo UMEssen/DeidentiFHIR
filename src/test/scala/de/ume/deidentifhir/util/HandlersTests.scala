95a2eabe65d0a29318e030d590335fc1d6f26daa
Tobias Brieden <tobias.brieden@uk-essen.de>
2022-05-06 08:51:10 +0200

    add a simple test for the referenceReplacementHandler

--- src/test/scala/de/ume/deidentifhir/util/HandlersTests.scala
+++ src/test/scala/de/ume/deidentifhir/util/HandlersTests.scala
@@ -1,33 +1,10 @@
 package de.ume.deidentifhir.util
 
-import org.hl7.fhir.r4.model.{Base, DateType, Patient, StringType}
+import ca.uhn.fhir.context.FhirContext
+import org.hl7.fhir.r4.model.{Base, DateType, Identifier, Observation, Patient, Reference, StringType}
 import org.scalatest.funsuite.AnyFunSuite
 
 class HandlersTests extends AnyFunSuite {
-
-//  // TODO move to HandlersTests
-//  test("generalizeDate") {
-//    val patient = new Patient()
-//    patient.setBirthDate(new Date())
-//    val pPatient : Patient = deidentifhir.deidentify(patient).asInstanceOf[Patient]
-//    val cal = Calendar.getInstance()
-//    cal.setTime(pPatient.getBirthDate)
-//    assert(cal.get(Calendar.DAY_OF_MONTH) == 15)
-//  }
-
-//  test("generalizeDeceased") {
-//
-//    val pDeceasedBoolean = generalizeDeceased(Seq(), new BooleanType(true))
-//    assert(pDeceasedBoolean.hasType("boolean"))
-//
-//    val pDeceasedDateType = generalizeDeceased(Seq(), new DateType())
-//    // TODO assert(pDeceasedDateType.hasType("DateType"))
-//
-//    val thrown = intercept[Exception] {
-//      generalizeDeceased(Seq(), new DateTimeType())
-//    }
-//    assert(thrown.getMessage === "received unexpected type!")
-//  }
 
   test("testStringReplacementHandler") {
     val pString = Handlers.stringReplacementHandler("test")(Seq(), new StringType("toBeReplaced"), Seq())
@@ -65,5 +42,21 @@ class HandlersTests extends AnyFunSuite {
         "test"
       }
     })(Seq("patient"), patient.getIdElement, Seq(patient))
+  }
+
+  test("referenceReplacementHandler") {
+
+    val idReplacementProvider = new IDReplacementProvider {
+      override def getIDReplacement(resourceType: String, id: String): String = {
+        assert(resourceType.equals("Patient"))
+        assert(id.equals("123"))
+        "456"
+      }
+    }
+
+    val obs = new Observation()
+    obs.setSubject(new Reference("Patient/123"))
+    val pRef = Handlers.referenceReplacementHandler(idReplacementProvider)(Seq("observation"), obs.getSubject.getReferenceElement_, Seq(obs))
+    assert(pRef.getValue.equals("Patient/456"))
   }
 }
